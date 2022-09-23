2e2f9b15def97818b01562914e7a849940dc3ff6
rabbitu04 <rabbitu04@gmail.com>
2022-09-23 11:23:14 +0800

    try update procfile to fix permission problem

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import os
+
 from src import create_app
 
 app = create_app()
@@ -6,3 +8,10 @@ app = create_app()
 @app.route('/hello-world', methods=['GET'])
 def hello_word():
     return 'Hello'
+
+
+if __name__ == '__main__':
+    app.run(
+        host='0.0.0.0',
+        port=int(os.getenv('FLASK_RUN_PORT', 5000))
+    )
