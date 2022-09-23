cb723cc2f8e1f63a5b30325b3945343f44707261
rabbitu04 <rabbitu04@gmail.com>
2022-09-23 11:36:35 +0800

    update port config for heroku

--- main.py
+++ main.py
@@ -13,5 +13,5 @@ def hello_word():
 if __name__ == '__main__':
     app.run(
         host='0.0.0.0',
-        port=int(os.getenv('FLASK_RUN_PORT', 5000))
+        port=int(os.getenv('PORT'))
     )
